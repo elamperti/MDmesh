bddc5c7fc07bfe711ace1b502f4a3c43690fdbde
Enrico Lamperti <mail@elamperti.com>
2015-01-30 02:08:59 -0300

    Added comments to the code. Better variable names. Fixed tiny bugs.

--- mdmesh.py
+++ mdmesh.py
@@ -69,32 +69,37 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # Creates the RegEx to match other md documents
     if args.follow_links:
-        inline_links = re.compile(r'(?<=\]\().+\.md(?=\))', re.MULTILINE)  # 
+        inline_links = re.compile(r'(?<=\]\().+\.md(?=\))', re.MULTILINE)
 
+    # Creates file queue and populates it
     file_queue = FileQueue()
     for file_pointer in args.files:
         full_path = os.path.abspath(file_pointer.name)
         file_queue.add(full_path, file_pointer)
 
-    md_mesh = ""
+    md_mesh = "" # where the compiled md is stored
+
     while file_queue.has_items():
-        (path, md_file) = file_queue.get()
+        (path, md_file_pointer) = file_queue.get()
         if args.use_separators and md_mesh:
             md_mesh += "\n\n---\n\n"
 
-        new_file = md_file.read()
-        md_file.close()
+        item_content = md_file_pointer.read()
+        md_file_pointer.close()
 
         if args.follow_links:
-            links = inline_links.findall(new_file)
+            links = inline_links.findall(item_content)
             if links:
-                base_path = os.path.dirname(os.path.abspath(md_file.name))
+                # Adds new documents to the queue, considering their full path
+                base_path = os.path.dirname(os.path.abspath(md_file_pointer.name))
                 for link in links:
-                    file_queue.add(base_path + '/' + link)
+                    file_queue.add(base_path + '/' + link) # FIXME: relative paths may cause duplicates
 
-        md_mesh += new_file
+        md_mesh += item_content
 
+    # Cleans filename and prepares the names for both possible outputs
     args.output = re.sub(r'\.(md|html)$', '', args.output, flags=re.IGNORECASE)
     md_path = args.output + '.md'
     html_path = args.output + '.html'
@@ -105,7 +110,9 @@ if __name__ == '__main__':
         md_output.close()
 
     if args.create_html:
+        #Parsing
         raw_html = markdown2.markdown(md_mesh, extras=['tables', 'fenced-code-blocks'])
+        
         html_output = open(html_path, 'w')
 
         if args.use_template:
@@ -114,6 +121,8 @@ if __name__ == '__main__':
             t.close()
 
             template = template.replace('{{ title }}', args.title)
+
+            # Puts the CSS inline
             if args.css:
                 try: 
                     css_file = open(args.css, 'r')
@@ -121,7 +130,8 @@ if __name__ == '__main__':
                     css_file.close()
                 except:
                     css = ""
-                template = template.replace('{{ css }}', css)
+                template = template.replace('{{ css }}', css, 1)
+
             template = template.replace('{{ content }}', raw_html)
 
             html_output.write(template)
